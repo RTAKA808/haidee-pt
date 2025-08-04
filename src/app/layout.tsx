f612487a4bfb79f061884ad1209104f71ac3021c
Reyn Takahashi <reyntakahashi@Reyns-MacBook-Pro.local>
2025-08-04 13:29:38 -0700

    initial app setup/importing of shadcn modules

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Haidee Sui Physical Therapy",
+  description: "Physical Therapy Scheduling and Consultation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button"
